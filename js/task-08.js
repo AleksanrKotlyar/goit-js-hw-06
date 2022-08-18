b1aceaa8cae1f4fe73a86a63298c6dd0948a9327
AleksanrKotlyar <kotlyar.alek@gmail.com>
2022-08-18 15:37:39 +0300

    8-9 task done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formRef = document.querySelector('.login-form');
+formRef.addEventListener('submit', onFormSubmit);
+let profile = {};
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
 
+  formData.forEach((value, name) => {
+    if (value === '') {
+      alert(`Все поля ${name} должны быть заполнены !`);
+    }
+    profile[name] = value;
+  });
+
+  event.currentTarget.reset();
+}
+console.log(profile);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const colorRef = document.querySelector('.color');
+
+btnRef.addEventListener('click', onBtnClickChangeColor);
+
+function onBtnClickChangeColor(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorRef.textContent = getRandomHexColor();
+}
